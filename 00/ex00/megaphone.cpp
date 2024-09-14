c9f4860dbe327f1a5e4aba9d92d2062acbe5617a
code-kiwi <madric.hottinger.101@gmail.com>
2024-09-14 06:51:08 +0200

    Completes ex00 from cpp00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,29 @@
 /*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:32:31 by mhotting          #+#    #+#             */
-/*   Updated: 2024/09/12 13:35:11 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/09/14 06:49:46 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char **argv) {
+	std::string	str;
+
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++) {
+		str = argv[i];
+		const size_t	str_len = str.length();
+		for (size_t j = 0; j < str_len; j++) {
+			str[j] = std::toupper(str[j]);
+		}
+		std::cout << str;
+	}
+	std::cout << std::endl;
+	return (0);
+}
