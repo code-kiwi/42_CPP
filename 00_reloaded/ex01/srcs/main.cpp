125f2ad15479b2d5971603859194379ca31dd930
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-01-16 19:46:52 +0100

    Adds files for reloaded CPP00 ex01

---
+++ 00_reloaded/ex01/srcs/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codekiwi <codekiwi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 19:43:34 by codekiwi          #+#    #+#             */
+/*   Updated: 2025/01/16 19:46:25 by codekiwi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "PhoneBook.hpp"
+
+int main(void) {
+    std::cout << "Hello PhoneBook" << std::endl;
+    return (0);
+}
