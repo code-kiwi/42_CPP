82f15bde721abcf7111e8092f3a02c18f840390c
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-02-11 15:28:29 +0100

    Continues ex00 from CPP03

--- 03/ex00/srcs/ClapTrap.cpp
+++ 03/ex00/srcs/ClapTrap.cpp
@@ -6,12 +6,13 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 22:30:59 by mhotting          #+#    #+#             */
-/*   Updated: 2025/02/10 23:01:27 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:27:51 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <limits>
 
 #include "ClapTrap.hpp"
 
@@ -32,19 +33,64 @@ ClapTrap::~ClapTrap(void) {
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& otherClapTrap) {
+    std::cout << "ClapTrap assignement operator called " << std::endl;
     if (this != &otherClapTrap) {
-        
+        this->_hitPoints = otherClapTrap._hitPoints;
+        this->_energyPoints = otherClapTrap._energyPoints;
+        this->_attackDamage = otherClapTrap._attackDamage;
     }
+    return *this;
 }
 
 void ClapTrap::attack(const std::string& target) {
-
+    if (this->_energyPoints == 0 || this->_hitPoints == 0) {
+        std::cout
+            << "ClapTrap " << this->_name
+            << " cannot attack " << target << "..."
+            << std::endl;
+        return;
+    }
+    this->_energyPoints--;
+    std::cout
+        << "ClapTrap " << this->_name
+        << " attacks " << target
+        << ", causing " << this->_attackDamage << " points of damage!"
+        << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
-
+    if (amount >= this->_hitPoints) {
+        this->_hitPoints = 0;
+        std::cout
+            << "ClapTrap " << this->_name
+            << " has been attacked and died..."
+            << std::endl;
+        return;
+    }
+    this->_hitPoints -= amount;
+    std::cout
+        << "ClapTrap " << this->_name
+        << " has been attacked and lost " << amount << " hit points!"
+        << " HEALTH: " << this->_hitPoints
+        << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-
+    if (this->_energyPoints == 0 || this->_hitPoints == 0) {
+        std::cout
+            << "ClapTrap " << this->_name
+            << " cannot be repaired..."
+            << std::endl;
+        return;
+    }
+    this->_energyPoints--;
+    if (this->_hitPoints > std::numeric_limits<unsigned int>::max() - amount) {
+        this->_hitPoints = std::numeric_limits<unsigned int>::max();
+    } else {
+        this->_hitPoints += amount;
+    }
+    std::cout
+        << "ClapTrap " << this->_name
+        << " has been repaired and got " << amount << " hit points!"
+        << std::endl;
 }
