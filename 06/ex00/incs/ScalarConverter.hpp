56caee400bb665c200ae55ec2e06e9bcb4cf60ac
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-06-02 16:24:06 +0200

    Adds controls to CPP06/ex00

--- 06/ex00/incs/ScalarConverter.hpp
+++ 06/ex00/incs/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 23:48:24 by mhotting          #+#    #+#             */
-/*   Updated: 2025/05/30 11:27:23 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/06/02 16:19:02 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ class ScalarConverter {
         ScalarConverter(const ScalarConverter&);
         ScalarConverter& operator=(const ScalarConverter&);
 
-        static void _printAsChar(const char c);
-        static void _printAsInt(const double nb);
-        static void _printAsFloat(const double nb);
-        static void _printAsDouble(const double nb);
+        static void _printAsChar(const double nb, const bool isChar, const bool isValidNumber);
+        static void _printAsInt(const double nb, const bool isValidNumber);
+        static void _printAsFloat(const double nb, const bool isValidNumber);
+        static void _printAsDouble(const double nb, const bool isValidNumber);
     public:
         ~ScalarConverter(void);
 
--- 06/ex00/srcs/ScalarConverter.cpp
+++ 06/ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 23:54:22 by mhotting          #+#    #+#             */
-/*   Updated: 2025/05/30 15:22:42 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/06/02 16:23:48 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,64 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter&) {
 ScalarConverter::~ScalarConverter(void) {}
 
 void ScalarConverter::convert(const std::string& toConvert) {
-    char* ptrToConvert;
+    char* ptrToConvert = NULL;
+    bool isChar = false;
+    bool isValidNumber = true;
+    double nb = 0;
 
-    std::cout << "Static Method, to convert: " << toConvert << std::endl;
-    double nb = strtod(toConvert.c_str(), &ptrToConvert);
-    std::cout << "nb: " << nb << std::endl;
-    std::cout << "STR: \"" << ptrToConvert << "\"" << std::endl;
-    std::cout << "Is Inf: " << std::isinf(nb) << std::endl;
-    std::cout << "Is Nan: " << std::isnan(nb) << std::endl;
+    // Checks if there is only one char to convert
+    if (toConvert.length() == 1 && !std::isdigit(toConvert[0])) {
+        isChar = true;
+    }
+    
+    nb = strtod(toConvert.c_str(), &ptrToConvert);
+    // Checks if the input is not a valid number
+    if (isChar || toConvert.length() == 0 || (*ptrToConvert != 0 && (*ptrToConvert != 'f' || *(ptrToConvert + 1) != 0))) {
+        isValidNumber = false;
+    }
+
+    // std::cout << "isChar: " << isChar << std::endl;
+    // std::cout << "isNumber: " << isValidNumber << std::endl;
+    // std::cout << "Static Method, to convert: " << toConvert << std::endl;
+    // std::cout << "nb: " << nb << std::endl;
+    // std::cout << "STR: \"" << ptrToConvert << "\"" << std::endl;
+    // std::cout << "Is Inf: " << std::isinf(nb) << std::endl;
+    // std::cout << "Is Nan: " << std::isnan(nb) << std::endl;
+    ScalarConverter::_printAsChar(nb, isChar, isValidNumber);
+    ScalarConverter::_printAsInt(nb, isValidNumber);
+    ScalarConverter::_printAsFloat(nb, isValidNumber);
+    ScalarConverter::_printAsDouble(nb, isValidNumber);
 }
 
-void ScalarConverter::_printAsChar(const char c) {
-    (void) c;
-}
-
-void ScalarConverter::_printAsInt(const double nb) {
+void ScalarConverter::_printAsChar(const double nb, const bool isChar, const bool isValidNumber) {
     (void) nb;
+    (void) isChar;
+    (void) isValidNumber;
+    std::cout << "char: ";
+
+    std::cout << std::endl;
 }
 
-void ScalarConverter::_printAsFloat(const double nb) {
+void ScalarConverter::_printAsInt(const double nb, const bool isValidNumber) {
     (void) nb;
+    (void) isValidNumber;
+    std::cout << "int: ";
+
+    std::cout << std::endl;
 }
 
-void ScalarConverter::_printAsDouble(const double nb) {
+void ScalarConverter::_printAsFloat(const double nb, const bool isValidNumber) {
     (void) nb;
+    (void) isValidNumber;
+    std::cout << "float: ";
+
+    std::cout << std::endl;
+}
+
+void ScalarConverter::_printAsDouble(const double nb, const bool isValidNumber) {
+    (void) nb;
+    (void) isValidNumber;
+    std::cout << "double: ";
+
+    std::cout << std::endl;
 }
