7aacd0403915779aba376044f05285c64ae0dd54
code-kiwi <madric.hottinger.101@gmail.com>
2025-06-29 23:11:15 +0200

    Ends CPP06/ex02 -> still to check

--- 06/ex02/srcs/main.cpp
+++ 06/ex02/srcs/main.cpp
@@ -6,13 +6,13 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 15:08:42 by mhotting          #+#    #+#             */
-/*   Updated: 2025/06/29 15:50:56 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/06/29 23:10:32 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 
 #include "Base.hpp"
 #include "A.hpp"
@@ -24,25 +24,69 @@ void identify(Base* p);
 void identify(Base& p);
 
 int main(void) {
-    Base b;
-    A a;
-    std::cout << "Hello" << std::endl;
+    Base* p;
 
     srand(time(NULL));
-    int nb = rand() % 3;
-    std::cout << "RANDOM: " << nb << std::endl;
+    p = generate();
+    identify(p);
+    identify(*p);
 
+    delete p;
     return 0;
 }
 
 Base* generate(void) {
+    int randNb;
 
-    return 0;
+    randNb = rand() % 3;
+    if (randNb == 0) {
+        return static_cast<Base*>(new A());
+    } else if (randNb == 1) {
+        return static_cast<Base*>(new B());
+    } else {
+        return static_cast<Base*>(new C());
+    }
 }
 
 void identify(Base* p) {
-    if (p == NULL)
-        return;
+    std::cout << "Identification pointer: ";
+
+    if (p == NULL) {
+        std::cout << "NULL pointer";
+    } else if (dynamic_cast<A*>(p)) {
+        std::cout << "A pointer";
+    } else if (dynamic_cast<B*>(p)) {
+        std::cout << "B pointer";
+    } else if (dynamic_cast<C*>(p)) {
+        std::cout << "C pointer";
+    } else {
+        std::cout << "Unknown pointer";
+    }
+
+    std::cout << std::endl;
     return;
 }
 
+void identify(Base& p) {
+    std::cout << "Identification reference: ";
+
+    try {
+        dynamic_cast<A&>(p);
+        std::cout << "A reference" << std::endl;
+        return ;
+    } catch (...) {}
+
+    try {
+        dynamic_cast<B&>(p);
+        std::cout << "B reference" << std::endl;
+        return ;
+    } catch (...) {}
+
+    try {
+        dynamic_cast<C&>(p);
+        std::cout << "C reference" << std::endl;
+        return ;
+    } catch (...) {}
+
+    std::cout << "Unknown reference";
+}
