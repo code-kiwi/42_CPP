d1c81663241cefae90a1984b97f6de23a48e70e6
code-kiwi <madric.hottinger.101@gmail.com>
2025-09-10 18:12:00 +0200

    Adds files to CPP08/ex02

---
+++ 08/ex02/incs/MutantStack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 17:57:30 by mhotting          #+#    #+#             */
+/*   Updated: 2025/09/10 18:07:34 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+};
+
+#endif
