e4cf2e63a836e9e2066791385b047deba5df0bab
mhotting <madric.hottinger.101@gmail.com>
2025-09-11 18:19:43 +0200

    CPP08/ex02: implements template and better inheritance handling

--- 08/ex02/incs/MutantStack.hpp
+++ 08/ex02/incs/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 17:57:30 by mhotting          #+#    #+#             */
-/*   Updated: 2025/09/11 17:29:55 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/09/11 18:18:43 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,20 @@ public:
   typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
   typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
   typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
-  
-  
+
+  iterator begin(void);
+  iterator end(void);
+
+  const_iterator begin(void) const;
+  const_iterator end(void) const;
+
+  reverse_iterator rbegin(void);
+  reverse_iterator rend(void);
+
+  const_reverse_iterator rbegin(void) const;
+  const_reverse_iterator rend(void) const;
 };
+
+#include "MutantStack.tpp"
 
 #endif
