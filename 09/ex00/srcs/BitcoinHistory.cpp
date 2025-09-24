18916f188a777818f346a6187af37a57bdbca302
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-09-24 14:50:28 +0200

    CPP09/ex00: adds parsing functions

--- 09/ex00/srcs/BitcoinHistory.cpp
+++ 09/ex00/srcs/BitcoinHistory.cpp
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 18:19:45 by mhotting          #+#    #+#             */
-/*   Updated: 2025/09/23 22:43:06 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/09/24 14:12:48 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,30 @@
 
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 
 BitcoinHistory::BitcoinHistory(const std::string &filename) {
     std::ifstream file(filename.c_str());
-    std::string line;
+    std::string line, key, value;
 
     if (!file.is_open()) {
         throw std::runtime_error("Impossible to open the given history file");
     }
+
+    // First line check
+    std::getline(file, line);
+    if (file.bad()) {
+        throw std::runtime_error("History file reading failed for first line");
+    }
+    trimStr(line);
+    if (line != EXPECTED_FIRST_LINE) {
+        throw std::runtime_error("History file's first line is incorrect");
+    }
+
+    // Extracting the map from the file
     while (std::getline(file, line)) {
-        size_t index;
-        trimStr(line);
-        
+        splitKeyValue(HISTORY_SEPARATOR, line, key, value);
+        std::cout << "line : '" << line << "' - key : '" << key << "' - value : '" << value << "'" << std::endl;
     }
     if (file.bad()) {
         throw std::runtime_error("History file reading failed");
--- 09/ex00/srcs/utils.cpp
+++ 09/ex00/srcs/utils.cpp
@@ -6,16 +6,15 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 20:09:12 by mhotting          #+#    #+#             */
-/*   Updated: 2025/09/23 20:34:55 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/09/24 14:49:57 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
 #include <cctype>
+#include <stdexcept>
 
-
-#include <iostream>
 void trimStr(std::string &s) {
     if (s.length() == 0) {
         return;
@@ -28,5 +27,50 @@ void trimStr(std::string &s) {
         end--;
     }
     s = s.substr(start, end - start + 1);
+}
 
+void splitKeyValue(char sep, const std::string &str, std::string &key, std::string &value) {
+    size_t index = str.find(sep);
+    if (index == std::string::npos) {
+        throw std::runtime_error("History file contains an incorrect line (seperator missing)");
+    }
+    key = str.substr(0, index);
+    trimStr(key);
+    value = str.substr(index + 1);
+    trimStr(value);
+}
+
+bool isStringDateValid(const std::string &str) {
+    // Len validation
+    if (str.length() != 10) {
+        return false;
+    }
+
+    // Content validation
+    for (size_t i = 0; i < str.length(); i++) {
+        if (i == 4 || i == 7) {
+            if (str[i] != '-')
+                return false;
+            continue;
+        }
+        if (isdigit(str[i]))
+            continue;
+        return false;
+    }
+
+    // Date validation
+    int year = std::atoi(str.substr(0, 4).c_str());
+    int month = std::atoi(str.substr(5, 2).c_str());
+    int day = std::atoi(str.substr(8, 2).c_str());
+}
+
+bool isDateValid(const int year, const int month, const int day) {
+    if (
+        year < 0 || year > 2025) {
+        return false;
+    }
+    return true;
+}
+
+double stringToDouble(const std::string &str) {
 }
