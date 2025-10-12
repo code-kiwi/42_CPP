38afa845821e495b27051252b702c66d816e7eec
CodeKiwiFR <madric.hottinger.101@gmail.com>
2025-10-12 20:28:02 +0200

    CPP09/ex00 : method of BitcoinExchange set as private

--- 09/ex00/incs/BitcoinExchange.hpp
+++ 09/ex00/incs/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 22:52:01 by mhotting          #+#    #+#             */
-/*   Updated: 2025/10/12 14:33:14 by mhotting         ###   ########.fr       */
+/*   Updated: 2025/10/12 20:26:53 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@
 class BitcoinExchange {
 public:
     static void parseInputFile(const std::string &fileName, const BitcoinHistory &history);
-    static void handleInputLine(const std::string &line, const BitcoinHistory &history);
 
 private:
     BitcoinExchange(void);
     BitcoinExchange(const BitcoinExchange &);
     ~BitcoinExchange(void);
     BitcoinExchange &operator=(const BitcoinExchange &);
+
+    static void handleInputLine(const std::string &line, const BitcoinHistory &history);
 };
 
 #endif
